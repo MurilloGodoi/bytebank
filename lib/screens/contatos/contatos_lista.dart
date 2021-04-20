da0204f90fd56f23b4627edfc5ace06d2122a1e6
MurilloGodoi <murillogodoi13@hotmail.com>
2021-04-20 00:43:21 -0300

    switching from stless to stful

--- lib/screens/contatos/contatos_lista.dart
+++ lib/screens/contatos/contatos_lista.dart
@@ -1,29 +1,61 @@
+import 'package:bytebank/database/dao/contato_dao.dart';
+import 'package:bytebank/models/Contato/contato.dart';
 import 'package:bytebank/screens/contatos/contatos_formulario.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ContatosLista extends StatelessWidget {
+class ContatosLista extends StatefulWidget {
+  @override
+  _ContatosListaState createState() => _ContatosListaState();
+}
+
+class _ContatosListaState extends State<ContatosLista> {
+  final ContatoDao _contatoDao = ContatoDao();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contatos'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Murillo',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              subtitle: Text(
-                '1000.0',
-                style: TextStyle(fontSize: 24.0),
-              ),
-            ),
-          )
-        ],
+      body: FutureBuilder<List<Contato>>(
+        initialData: List(),
+        future: Future.delayed(Duration(seconds: 1)).then(
+          (value) => _contatoDao.findAll(),
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Text('Carregando...'),
+                  ],
+                ),
+              );
+              break;
+
+            case ConnectionState.active:
+              break;
+
+            case ConnectionState.done:
+              final List<Contato> contatos = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contato contato = contatos[index];
+                  return _ItemContato(contato);
+                },
+                itemCount: contatos.length,
+              );
+              break;
+          }
+          return Text('Erro desconhecido');
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -33,10 +65,34 @@ class ContatosLista extends StatelessWidget {
                   builder: (context) => FormularioContatos(),
                 ),
               )
-              .then((contato) => debugPrint(contato.nome));
+              .then(
+                (value) => setState(() {}),
+              );
         },
         child: Icon(
           Icons.add,
+        ),
+      ),
+    );
+  }
+}
+
+class _ItemContato extends StatelessWidget {
+  final Contato contato;
+
+  const _ItemContato(this.contato);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contato.nome,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contato.numeroConta.toString(),
+          style: TextStyle(fontSize: 24.0),
         ),
       ),
     );
